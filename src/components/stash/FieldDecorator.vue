bbbc85d56f66b4dadd0ad222deed0cc468e79d5a
Adam Clark <adam.h.clark@gmail.com>
2018-05-04 23:13:47 -0700

    Stashing some things

---
+++ src/components/stash/FieldDecorator.vue
@@ -0,0 +1,27 @@
+<!--
+This wraps the field with a label, help text, etc.
+-->
+<template>
+  <div>
+    <slot name="label">
+      <label>{{ context.label }}</label>
+    </slot>
+    <slot>
+      (Input goes here)
+    </slot>
+    <slot name="extra">
+      <div v-if="context.helpText">{{ context.helpText }}</div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldDecorator',
+  props: ['context'],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
